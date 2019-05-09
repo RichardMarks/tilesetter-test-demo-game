cd2d9437f620e61e3ce84331fa958dc91380a3fc
Richard Marks <ccpsceo@gmail.com>
2019-05-08 22:51:51 -0500

    moved hard-coded level data into external json, added level loader, and items (no item collisions yet)

--- main.js
+++ main.js
@@ -1,70 +1,11 @@
 (function (RML) {
   const TILE_WIDTH = 16
   const TILE_HEIGHT = 16
-  const MAP_WIDTH = 30
-  const MAP_HEIGHT = 20
-  const MAP_DATA = [
-    17, 24, 24, 24, 24, 24, 24, 24, 24, 24,
-    24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
-    24, 24, 24, 24, 24, 24, 24, 24, 24, 18,
-    14, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 12,
-    14, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 12,
-    14, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 12,
-    14, 0, 0, 0, 0, 0, 0, 0, 0, 34,
-    35, 36, 0, 0, 0, 34, 35, 36, 0, 15,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 12,
-    14, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 38,
-    35, 36, 0, 0, 0, 0, 0, 0, 0, 12,
-    14, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 4, 0, 0, 0, 0, 0, 0, 12,
-    14, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 15, 0, 0, 0, 0, 0, 0, 12,
-    14, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 26, 0, 0, 0, 0, 0, 0, 12,
-    14, 34, 35, 36, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 34, 35, 36, 0, 12,
-    14, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 1, 2, 3, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 12,
-    14, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 12, 13, 14, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 12,
-    14, 0, 0, 0, 0, 1, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 29, 13, 28, 2, 2,
-    2, 2, 2, 3, 0, 0, 0, 0, 0, 12,
-    14, 0, 0, 0, 0, 12, 13, 13, 13, 13,
-    13, 13, 13, 13, 13, 13, 13, 13, 13, 13,
-    13, 13, 13, 14, 0, 0, 0, 0, 0, 12,
-    14, 0, 0, 0, 0, 12, 13, 13, 13, 13,
-    13, 13, 13, 13, 13, 13, 13, 13, 13, 13,
-    13, 13, 13, 14, 0, 0, 0, 0, 0, 12,
-    14, 0, 0, 0, 0, 23, 24, 24, 24, 24,
-    24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
-    24, 24, 24, 25, 0, 0, 0, 0, 0, 12,
-    14, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 12,
-    14, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 12,
-    14, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 12,
-    14, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 12
-  ]
+  const SCREEN_WIDTH = 30 * TILE_WIDTH
+  const SCREEN_HEIGHT = 20 * TILE_HEIGHT
+
+  // FIXME: read from an environment variable or something
+  const devmode = true
 
   const { min: mathMin, max: mathMax, abs: mathAbs } = Math
 
@@ -72,7 +13,17 @@
   const clamp = (v, low, high) => mathMax(low, mathMin(high, v))
   const lerp = (a, b, t) => a * (1.0 - t) + b * t
 
-  const screen = RML.Screen.create(MAP_WIDTH * TILE_WIDTH, MAP_HEIGHT * TILE_HEIGHT)
+  const screen = RML.Screen.create(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+  const messages = {
+    queue: [],
+    peek: () => messages.queue.length ? messages.queue[messages.queue.length - 1] : null,
+    next: () => messages.queue.length ? messages.queue.pop() : null,
+    add (type, ...params) {
+      const message = { type, data: {...params} }
+      messages.queue = [message, ...messages.queue]
+    }
+  }
 
   const input = {
     up: false,
@@ -101,6 +52,11 @@
             // console.log('mapped key press', which, input.kb.mapping[which])
           }
         } else {
+          // R key to restart in dev mode
+          if (devmode && which === 82) {
+            messages.add('restart')
+            return
+          }
           console.log('unmapped key press', which)
         }
       },
@@ -133,6 +89,135 @@
 
   const resources = { images: {} }
 
+  const level = {
+    load (levelNumber) {
+      const levelId = `level${levelNumber}`
+      if (levelId in resources.levels) {
+        const data = level.parseLevel(resources.levels[levelId], levelNumber)
+        return data
+      }
+      return null
+    },
+
+    parseLevel (levelJSON, levelNumber) {
+      const {
+        height,
+        items,
+        name,
+        playerX,
+        playerY,
+        solids,
+        tiles,
+        width
+      } = levelJSON
+
+      console.log(`loading level ${levelNumber} [${name}] ...`)
+      demo.level.number = levelNumber
+      demo.level.name = name
+
+      // load tilemap data
+      demo.level.width = width
+      demo.level.height = height
+      demo.level.tiles = [...tiles]
+      console.log(`level size ${width} x ${height}`)
+      console.log(`tiles: ${tiles.length} / ${width * height}`)
+
+      // load collision data
+      console.log(`loading collision data... ${solids.length} collidables`)
+      demo.world.solids = [...solids]
+
+      // load item data
+      console.log(`loading item data... ${items.length} items`)
+      demo.items.instances = items.map((item, index) => ({ ...item, id: index }))
+
+      // position the player
+      console.log(`positioning player at ${playerX}, ${playerY}`)
+      demo.playerCollider.x = playerX
+      demo.playerCollider.y = playerY
+
+      console.log('load complete')
+    }
+  }
+
+  const animation = {
+    ANIM_LOOP: 0,
+    ANIM_PINGPONG: 1,
+    ANIM_ONCE: 2,
+
+    create ({ name, duration = 1, frames, type = 0, onComplete, onLoop, onPing, onPong }) {
+      const anim = {
+        name,
+        type,
+        duration: duration / frames.length,
+        playing: false,
+        complete: false,
+        ping: false,
+        time: 0,
+        frame: 0,
+        frames: frames.slice()
+      }
+
+      const tickAnim = deltaTime => {
+        anim.time += deltaTime
+        return anim.time >= anim.duration
+      }
+
+      const nextFrame = () => {
+        anim.frame += 1
+        return anim.frame >= anim.frames.length
+      }
+
+      const previousFrame = () => {
+        anim.frame -= 1
+        return anim.frame < 0
+      }
+
+      if (type === animation.ANIM_LOOP) {
+        anim.update = deltaTime => {
+          if (anim.playing) {
+            if (tickAnim(deltaTime)) {
+              anim.time = 0
+              if (nextFrame()) {
+                anim.frame = 0
+                onLoop && onLoop()
+              }
+            }
+          }
+        }
+      } else if (type === animation.ANIM_PINGPONG) {
+        anim.update = deltaTime => {
+          if (anim.playing) {
+            if (tickAnim(deltaTime)) {
+              const frameChange = anim.ping ? previousFrame : nextFrame
+              if (frameChange()) {
+                anim.frame = anim.ping ? anim.frames.length - 1 : 0
+                anim.ping = !anim.ping
+                anim.ping && onPing && onPing()
+                !anim.ping && onPong && onPong()
+              }
+            }
+          }
+        }
+      } else if (type === animation.ANIM_ONCE) {
+        anim.update = deltaTime => {
+          if (anim.playing) {
+            if (tickAnim(deltaTime)) {
+              anim.time = 0
+              if (nextFrame()) {
+                anim.frame = anim.frames.length - 1
+                anim.playing = false
+                anim.complete = true
+                onComplete && onComplete()
+              }
+            }
+          }
+        }
+      }
+
+      return anim
+    }
+  }
+
   const preload = (manifest) => new Promise((resolve, reject) => {
     console.log('preloading...')
     let loaded = 0
@@ -151,6 +236,28 @@
           }
         }
         imageResource.src = spec.src
+      } else if (spec.type === 'json') {
+        const req = new XMLHttpRequest()
+        req.onreadystatechange = () => {
+          if (req.readyState === 4) {
+            if (req.status === 200) {
+              if (! ('levels' in resources)) {
+                resources.levels = {}
+              }
+              resources.levels[spec.id] = JSON.parse(req.responseText)
+              loaded += 1
+              console.log(`loaded resource ${loaded} of ${toLoad}`)
+              if (loaded >= toLoad) {
+                console.log('all resources loaded')
+                resolve()
+              }
+            } else {
+              reject(new Error(`Failed to load ${spec.id} from ${spec.src} Status Code: ${req.status}`))
+            }
+          }
+        }
+        req.open('GET', spec.src, true)
+        req.send()
       }
     })
   })
@@ -172,108 +279,43 @@
 
     const { blit } = screen
 
+    demo.level = {
+      width: 0,
+      height: 0,
+      tiles: [],
+      name: '',
+      number: 0
+    }
+
     demo.tilemap = {
       draw () {
-        const Z = 1
-        const mapSize = MAP_DATA.length
+        const {
+          width: levelWidth,
+          // height: levelHeight,
+          tiles: levelTiles
+        } = demo.level
+
+        const mapSize = levelTiles.length
         const { tileset } = demo
+        const image = resources.images.tileset
+
         for (let i = 0; i < mapSize; i += 1) {
-          const tileId = MAP_DATA[i]
+          const tileId = levelTiles[i]
           if (tileId !== 0) {
             const { srcX, srcY, srcW, srcH } = tileset[tileId]
-            const dstX = ~~(i % MAP_WIDTH) * (TILE_WIDTH * Z)
-            const dstY = ~~(i / MAP_WIDTH) * (TILE_HEIGHT * Z)
-            const image = resources.images.tileset
+            const dstX = ~~(i % levelWidth) * TILE_WIDTH
+            const dstY = ~~(i / levelWidth) * TILE_HEIGHT
 
             blit({
               image,
               srcX, srcY,
               srcW, srcH,
               dstX, dstY,
-              dstW: TILE_WIDTH * Z,
-              dstH: TILE_HEIGHT * Z
+              dstW: TILE_WIDTH,
+              dstH: TILE_HEIGHT
             })
           }
         }
-      }
-    }
-
-    const animation = {
-      ANIM_LOOP: 0,
-      ANIM_PINGPONG: 1,
-      ANIM_ONCE: 2,
-
-      create ({ name, duration = 1, frames, type = 0, onComplete, onLoop, onPing, onPong }) {
-        const anim = {
-          name,
-          type,
-          duration: duration / frames.length,
-          playing: false,
-          complete: false,
-          ping: false,
-          time: 0,
-          frame: 0,
-          frames: frames.slice()
-        }
-
-        const tickAnim = deltaTime => {
-          anim.time += deltaTime
-          return anim.time >= anim.duration
-        }
-
-        const nextFrame = () => {
-          anim.frame += 1
-          return anim.frame >= anim.frames.length
-        }
-
-        const previousFrame = () => {
-          anim.frame -= 1
-          return anim.frame < 0
-        }
-
-        if (type === animation.ANIM_LOOP) {
-          anim.update = deltaTime => {
-            if (anim.playing) {
-              if (tickAnim(deltaTime)) {
-                anim.time = 0
-                if (nextFrame()) {
-                  anim.frame = 0
-                  onLoop && onLoop()
-                }
-              }
-            }
-          }
-        } else if (type === animation.ANIM_PINGPONG) {
-          anim.update = deltaTime => {
-            if (anim.playing) {
-              if (tickAnim(deltaTime)) {
-                const frameChange = anim.ping ? previousFrame : nextFrame
-                if (frameChange()) {
-                  anim.frame = anim.ping ? anim.frames.length - 1 : 0
-                  anim.ping = !anim.ping
-                  anim.ping && onPing && onPing()
-                  !anim.ping && onPong && onPong()
-                }
-              }
-            }
-          }
-        } else if (type === animation.ANIM_ONCE) {
-          anim.update = deltaTime => {
-            if (anim.playing) {
-              if (tickAnim(deltaTime)) {
-                anim.time = 0
-                if (nextFrame()) {
-                  anim.frame = anim.frames.length - 1
-                  anim.playing = false
-                  anim.complete = true
-                  onComplete && onComplete()
-                }
-              }
-            }
-          }
-        }
-
-        return anim
       }
     }
 
@@ -359,22 +401,49 @@
       }
     }
 
+    demo.items = {
+      instances: [],
+      update (deltaTime) {
+        demo.items.instances.forEach(instance => demo.items.updateInstance(instance, deltaTime))
+      },
+      draw () {
+        demo.items.instances.forEach(demo.items.drawInstance)
+      },
+      updateInstance (instance, deltaTime) {
+
+      },
+      drawInstance (instance) {
+        const { type, x, y } = instance
+        const image = resources.images[type]
+
+        blit({
+          image,
+          srcX: 0,
+          srcY: 0,
+          srcW: image.width,
+          srcH: image.height,
+          dstX: ~~(x),
+          dstY: ~~(y),
+          dstW: 16,
+          dstH: 16
+        })
+      },
+      drawDebug () {
+        const { instances } = demo.items
+
+        screen.ctx.globalAlpha = 0.3
+        screen.ctx.fillStyle = '#f00'
+        for (let i = 0; i < instances.length; i += 1) {
+          const { x, y } = instances[i]
+          screen.ctx.fillRect(x, y, 16, 16)
+        }
+        screen.ctx.fillStyle = '#000'
+        screen.ctx.globalAlpha = 1
+      }
+    }
+
     demo.world = {
-      solids: [
-        // todo - parse map data to build the collision objects instead of harc-coding them
-        [0, 0, 16 * MAP_WIDTH, 16],
-        [0, 16, 16, 16 * (MAP_HEIGHT - 1)],
-        [16 * (MAP_WIDTH - 1), 16, 16, 16 * (MAP_HEIGHT - 1)],
-        [16, 16 * 9, 16 * 3, 16],
-        [16 * 9, 16 * 4, 16 * 3, 16],
-        [16 * 15, 16 * 4, 16 * 3, 16],
-        [16 * 19, 16 * 5, 16 * 3, 16],
-        [16 * 19, 16 * 3, 16, 16 * 2],
-        [16 * 22, 16 * 6, 16, 16 * 3],
-        [16 * 25, 16 * 9, 16 * 3, 16],
-        [16 * 5, 16 * 12, 16 * 19, 16 * 4],
-        [16 * 15, 16 * 10, 16 * 3, 16 * 2]
-      ],
+      solids: [],
       ptCollides (x, y) {
         const { solids } = demo.world
         const { length: count } = solids
@@ -521,6 +590,10 @@
 
         // update animation
         animationChange && player.setAnimation(nextAnim)
+
+        if (collider.y > screen.height) {
+          messages.add('restart')
+        }
       },
 
       drawDebug () {
@@ -560,19 +633,68 @@
   demo.start = () => {
     console.log('demo start')
     demo.player.animations[demo.player.currentAnimationName].playing = true
+    level.load(1)
   }
 
   demo.update = deltaTime => {
     demo.playerCollider.update(deltaTime)
     demo.player.update(deltaTime)
+    demo.items.update(deltaTime)
   }
 
   demo.draw = () => {
     screen.clear()
     demo.tilemap.draw()
+    demo.items.draw()
     demo.player.draw()
+    // demo.items.drawDebug()
     // demo.world.drawDebug()
     // demo.playerCollider.drawDebug()
+  }
+
+  demo.restart = () => {
+    level.load(demo.level.number)
+
+    // reset the player collider
+    // FIXME: this is bad practice copy/pasting code
+    const platformReset = {
+      gravity: 300,
+      maxSpeed: 120,
+      jumpSpeed: 160,
+      acceleration: 600,
+      xSpeed: 0,
+      ySpeed: 0,
+      onGround: false,
+      isJumping: false
+    }
+    // const colliderReset = {
+    //   x: (screen.width / 2) - 160,
+    //   y: (screen.height / 2) - 64,
+    // }
+    // apply resets
+    demo.playerCollider.platform = {...platformReset}
+    // demo.playerCollider = {...demo.playerCollider, ...colliderReset}
+
+    // reset the player
+    Object.keys(demo.player.animations).forEach(k => {
+      const anim = demo.player.animations[k]
+      anim.playing = false
+      anim.frame = 0
+    })
+    demo.player.currentAnimationName = 'idle'
+    demo.player.mirror = false
+  }
+
+  const handleMessage = message => {
+    console.log('handleMessage got message')
+    console.log({ message })
+
+    if (message.type === 'restart') {
+      demo.restart()
+      return true
+    }
+
+    return false
   }
 
   const start = () => {
@@ -582,6 +704,12 @@
     let lastTime = getTicks()
     demo.start && demo.start()
     const mainLoop = () => {
+      const nextMessage = messages.peek()
+      if (nextMessage) {
+        if (handleMessage(nextMessage)) {
+          messages.next()
+        }
+      }
       const currentTime = getTicks()
       const deltaTime = (currentTime - lastTime) * 0.001
       demo.update && demo.update(deltaTime)
@@ -596,8 +724,16 @@
     console.log('DOM ready')
     const manifest = [
       { id: 'tileset', type: 'image', src: 'set-1.png' },
-      { id: 'player', type: 'image', src: 'player.png' }
+      { id: 'player', type: 'image', src: 'player.png' },
+      { id: 'fruit', type: 'image', src: 'fruit.png' },
+      { id: 'level1', type: 'json', src: 'level1.json' }
     ]
     preload(manifest).then(create).then(start)
+
+    window.gamedebugobjs = {
+      screen,
+      demo,
+      resources
+    }
   })
 })(window.RML || {})
